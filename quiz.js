c19230d4e60f365dc4264b097461a260fb4b7b08
Trish Tyra <patrycjatyra@protonmail.comgit init project1>
2019-09-21 20:06:08 +0100

    variables counting points

--- quiz.js
+++ quiz.js
@@ -1,2 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
