a8ea4efcdd41f9baede089c23b939f03e9d7e054
Trish Tyra <patrycjatyra@protonmail.comgit init project1>
2019-09-22 23:31:36 +0100

    added set item local storgae to js

--- quiz.js
+++ quiz.js
@@ -51,7 +51,7 @@ startGame = () => {
 
   getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-     
+        localStorage.setItem("mostRecentScore", score);
       return window.location.assign("/end.html");
     }
     questionCounter++;
