7c96961242e2197671fedbaf2e44b0071a2c0c82
Trish Tyra <patrycjatyra@protonmail.comgit init project1>
2019-09-23 10:15:50 +0100

    checked code because of major errors

--- end.js
+++ end.js
@@ -1,4 +1,3 @@
-  
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
@@ -9,6 +8,7 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
+
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
 });
